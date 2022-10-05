e198bd67901f64a6ff411fe86452034e9d0a092c
Eugene <rezak97@mail.ru>
2022-10-05 14:44:39 +0800

    0.1.5 - Правки по дизайну

--- app/src/main/java/com/example/myapplication/DetailsFilmActivity.kt
+++ app/src/main/java/com/example/myapplication/DetailsFilmActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myapplication
 
-
+import android.content.Intent
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.DetailsFilmActivityBinding
-import com.example.myapplication.model.FilmModel
 
 class DetailsFilmActivity : AppCompatActivity() {
 
@@ -16,24 +18,36 @@ class DetailsFilmActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding =
             DetailsFilmActivityBinding.inflate(layoutInflater).also { setContentView(it.root) }
-        binding.secondFilmName.findViewById<TextView>(R.id.secondFilmName).also { onGet() }
+        binding.detailsFilmName.findViewById<TextView>(R.id.detailsFilmName).also { onGet() }
 
-        binding.secondFilmDescription.findViewById<TextView>(R.id.secondFilmDescription)
+        binding.detailsFilmDescription.findViewById<TextView>(R.id.detailsFilmDescription)
             .also { onGet() }
 
-        binding.secondPhotoImageView.findViewById<ImageView>(R.id.secondPhotoImageView)
+        binding.detailsPhotoImageView.findViewById<ImageView>(R.id.detailsPhotoImageView)
             .also { onGet() }
+
+        binding.buttonBack.findViewById<Button>(R.id.buttonBack).also { goBack() }
+
     }
 
     private fun onGet() {
-       // binding.secondFilmName.setText(intent.getStringExtra())
-        binding.secondFilmName.setText(intent.getCharSequenceExtra(FILM_NAME))
-        binding.secondFilmDescription.setText(intent.getCharSequenceExtra(FILM_DESCRIPTION))
-        binding.secondPhotoImageView.setTag(intent.getStringExtra(FILM_IMAGE))
+        Glide.with(binding.detailsPhotoImageView)
+            .load(intent.getStringExtra(FILM_IMAGE))
+            .into(binding.detailsPhotoImageView)
+
+        binding.detailsFilmName.setText(intent.getStringExtra(FILM_NAME))
+        binding.detailsFilmDescription.setText(intent.getStringExtra(FILM_DESCRIPTION))
+    }
+
+    fun goBack() {
+        binding.buttonBack.setOnClickListener {
+            finish()
+        }
+
     }
 
     companion object {
-        const val ID = "ID"
+        //   const val ID = "ID"
         const val FILM_NAME = "FILM NAME"
         const val FILM_DESCRIPTION = "FILM DESCRIPTION"
         const val FILM_IMAGE = "FILM IMAGE"
